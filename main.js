69cf33ec91b690ef07538e97c939a4856360533a
peakcary <peakcary@163.com>
2019-10-02 16:45:33 +0800

    init TY-1-4

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+require('./main.css');
 const show = require('./show.js');
 show('peak');
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,15 @@ module.exports = {
     entry:'./main.js',
     output:{
         filename:'bundle.js',
+        
         path:path.resolve(__dirname,'./dist'),
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader'],
+            }
+        ]
     }
 }
